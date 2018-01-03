310121725ef8b78625da2dc918e7771dd5d5f2a2
KryniuPL <krzysztof.draganfm@gmail.com>
2018-01-03 20:29:31 +0100

    uploadPhotosFeature

--- src/main/java/com/library/config/LibraryWebAppInitializer.java
+++ src/main/java/com/library/config/LibraryWebAppInitializer.java
@@ -1,6 +1,17 @@
 package com.library.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
+import java.io.IOException;
 
 public class LibraryWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
 
@@ -15,5 +26,23 @@ public class LibraryWebAppInitializer extends AbstractAnnotationConfigDispatcher
     @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class<?>[] { WebConfig.class };
+    }
+
+    @Override
+    protected void customizeRegistration(Dynamic registration)
+    {
+       // DispatcherServlet ds=new DispatcherServlet();
+        //registration=context.add
+        registration.setMultipartConfig(new MultipartConfigElement("kryniu/Desktop/library_application/uploads",2097152,4194304,0));
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() throws IOException
+    {
+        CommonsMultipartResolver multipartResolver=new CommonsMultipartResolver();
+        multipartResolver.setUploadTempDir(new FileSystemResource("kryniu/Desktop/library_application/uploads"));
+        multipartResolver.setMaxUploadSize(2097152);//2 MB MEMORY
+        multipartResolver.setMaxInMemorySize(0);
+        return multipartResolver;
     }
 }
