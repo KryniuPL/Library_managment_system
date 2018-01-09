fcb82524792a4c5aeea748c4de3309ba293a4721
Damian-Lenovo <333damian333@gmail.com>
2018-01-09 21:09:37 +0100

    Changed jdbc version

--- src/main/java/com/library/config/SecurityConfig.java
+++ src/main/java/com/library/config/SecurityConfig.java
@@ -1,9 +1,9 @@
 package com.library.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 /*
