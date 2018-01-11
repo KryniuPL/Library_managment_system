00ffe80d17e26cacb461c4ca3fee515cce35a15c
Damian-Lenovo <333damian333@gmail.com>
2018-01-11 09:05:34 +0100

    Adding new books works now
    Bugs: mapping for addbooks in POST method is not working 'couse view is missing right now
    Note: use /allbooks URL to view DB content

--- src/main/java/com/library/controllers/BooksController.java
+++ src/main/java/com/library/controllers/BooksController.java
@@ -5,10 +5,15 @@ import com.library.model.Book;
 import com.library.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -18,14 +23,20 @@ public class BooksController {
     private BookRepository bookRepository;
 
     @RequestMapping(value = "/addbook", method = RequestMethod.GET)
-    public String showAddBookForm() {
+    public String showAddBookForm(Model model) {
+        model.addAttribute("book", new Book());
         return "addbook";
     }
 
     @RequestMapping(value = "/addbook", method = RequestMethod.POST)
-    public String addBook(Book book){
+    public String addBook(@Valid @ModelAttribute("book") Book book, Model model, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return "addbook";
+        }
+        model.addAttribute("author",book.getAuthor());
+        model.addAttribute("name",book.getName());
         bookRepository.save(book);
-        return "addbook";
+        return "allbooks";
     }
 
     @RequestMapping(value = "/allbooks", method = RequestMethod.GET)
