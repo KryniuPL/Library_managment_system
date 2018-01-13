12861886262a508ce769f09ae0faa823c236fdf4
Damian-Lenovo <333damian333@gmail.com>
2018-01-14 00:30:00 +0100

    DB doesnt't drop after exit from now

--- src/main/java/com/library/config/DatabaseConfig.java
+++ src/main/java/com/library/config/DatabaseConfig.java
@@ -64,7 +64,7 @@ public class DatabaseConfig {
 
     Properties additionalProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         return properties;
     }
