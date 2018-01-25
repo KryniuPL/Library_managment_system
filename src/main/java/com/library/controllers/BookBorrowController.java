d5ab40ffbf5afc820050884458eb421e4b13f85a
KryniuPL <krzysztof.draganfm@gmail.com>
2018-01-25 14:26:42 +0100

    BorrowingBook implementation part1

--- src/main/java/com/library/controllers/BookBorrowController.java
+++ src/main/java/com/library/controllers/BookBorrowController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
+
 @Controller
 public class BookBorrowController{
 
@@ -26,6 +28,18 @@ public class BookBorrowController{
     public String updateBook(@PathVariable("id") Long id, Model model){
         model.addAttribute("book",bookRepository.findOne(id));
         return "bookborrowForm";
+    }
+
+    @RequestMapping(value = "/bookborrow/save", method = RequestMethod.POST)
+    public String saveBorrowBook(@Valid @ModelAttribute("bookborrow") BookBorrow bookBorrow,Model model)
+    {
+
+        model.addAttribute("startDate",bookBorrow.getStartDate());
+        model.addAttribute("endDate",bookBorrow.getEndDate());
+        model.addAttribute("bookID",bookBorrow.getBookID());
+        model.addAttribute("userID",bookBorrow.getEndDate());
+        bookBorrowRepository.save(bookBorrow);
+        return "redirect:/allbooks";
     }
 
 
