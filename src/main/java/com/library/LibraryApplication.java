a944e7920e3838fe8bd8e1ca4dde2846c08b23a7
Damian-Lenovo <333damian333@gmail.com>
2018-01-30 13:52:52 +0100

    Scheduler works

--- src/main/java/com/library/LibraryApplication.java
+++ src/main/java/com/library/LibraryApplication.java
@@ -2,10 +2,14 @@ package com.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Główna klasa uruchamiająca aplikację
  */
+@Configuration
+@EnableScheduling
 @SpringBootApplication
 public class LibraryApplication {
 
--- src/main/java/com/library/scheduler/BookBorrowScheduler.java
+++ src/main/java/com/library/scheduler/BookBorrowScheduler.java
@@ -3,19 +3,30 @@ package com.library.scheduler;
 import com.library.model.BookBorrow;
 import com.library.repository.BookBorrowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@EnableScheduling
+/**
+ * Wykonuje zaplanowane metody na obiektach BookBorrow
+ */
+
+@Component
 public class BookBorrowScheduler {
 
     @Autowired
     private BookBorrowRepository bookBorrowRepository;
 
-    @Scheduled(cron = "0 0 0 1/1 * ? *")
-    public void checkBorrows(){
+    /**
+     * Codziennie o północy sprawdza daty wypożyczeń
+     *
+     * W tym celu wykorzystuje wzorzec obserwatora,
+     * który powiadamia poszczególnych użytkowników
+     * o tym, że termin oddania dobiega końca lub został przekroczony
+     */
+    @Scheduled(cron = "0 0 0 ? * * ")
+    public void checkBorrowsDates(){
         List<BookBorrow> bookBorrowList = bookBorrowRepository.findAll();
         for (BookBorrow bookBorrow: bookBorrowList)
             bookBorrow.checkDate();
