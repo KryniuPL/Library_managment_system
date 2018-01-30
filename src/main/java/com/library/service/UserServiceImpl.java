e1c6973fa0ea2f2fadc9203fa6eff3b7865c627b
KryniuPL <krzysztof.draganfm@gmail.com>
2018-01-30 23:34:18 +0100

    Final commit, GG WP Guys

--- src/main/java/com/library/service/UserServiceImpl.java
+++ src/main/java/com/library/service/UserServiceImpl.java
@@ -49,7 +49,7 @@ public class UserServiceImpl implements UserService {
     {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setActive(1);
-        Role userRole=roleRepository.findByName("ADMIN");
+        Role userRole=roleRepository.findByName("USER");
         user.setRoles(new HashSet<>(Arrays.asList(userRole)));
         userRepository.save(user);
     }
