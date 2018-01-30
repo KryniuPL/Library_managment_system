042c78b00ba12a67c823717db0bcff5d50547159
Damian-Lenovo <333damian333@gmail.com>
2018-01-30 09:31:08 +0100

    scheduler fix

--- src/main/java/com/library/scheduler/BookBorrowScheduler.java
+++ src/main/java/com/library/scheduler/BookBorrowScheduler.java
@@ -3,13 +3,11 @@ package com.library.scheduler;
 import com.library.model.BookBorrow;
 import com.library.repository.BookBorrowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.List;
 
-@Configuration
 @EnableScheduling
 public class BookBorrowScheduler {
 
